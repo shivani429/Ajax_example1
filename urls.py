9001bd601fdd6e4133322127cbc18a62fd2877d1
shivani429 <113819244+shivani429@users.noreply.github.com>
2022-10-25 16:56:54 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from django.views.generic.base import TemplateView
 from demoapp.views import *
 urlpatterns = [
     
